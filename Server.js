1a14076d680c54bdc7442009ebf8650f65170209
apple <apple@apples-mbp.eduroam.hs-furtwangen.de>
2018-06-14 11:42:54 +0200

    .

--- Server.js
+++ Server.js
@@ -1,51 +1,71 @@
-/**
- * Simple server managing between client and database
- * @author: Jirka Dell'Oro-Friedl
- */
 "use strict";
 const Http = require("http");
 const Url = require("url");
 const Database = require("./Database");
-console.log("Server starting");
 let port = process.env.PORT;
 if (port == undefined)
-    port = 8100;
+    port = 8200;
 let server = Http.createServer();
-server.addListener("listening", handleListen);
 server.addListener("request", handleRequest);
 server.listen(port);
-function handleListen() {
-    console.log("Listening on port: " + port);
-}
-function handleRequest(_request, _response) {
-    console.log("Request received");
-    let query = Url.parse(_request.url, true).query;
-    var command = query["command"];
-    switch (command) {
-        case "insert":
-            let student = {
-                name: query["name"],
-                firstname: query["firstname"],
-                matrikel: parseInt(query["matrikel"])
-            };
-            Database.insert(student);
-            respond(_response, "storing data");
-            break;
-        case "refresh":
-            Database.findAll(function (json) {
-                respond(_response, json);
-            });
-            break;
-        default:
-            respond(_response, "unknown command: " + command);
-            break;
-    }
-}
 function respond(_response, _text) {
-    //console.log("Preparing response: " + _text);
-    _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
     _response.write(_text);
     _response.end();
+}
+function handleRequest(_request, _response) {
+    console.log("Ich höre Stimmen!");
+    let query = Url.parse(_request.url, true).query;
+    console.log(query["command"]);
+    if (query["command"]) {
+        switch (query["command"]) {
+            case "insert":
+                insert(query, _response);
+                break;
+            case "refresh":
+                refresh(_response);
+                break;
+            case "search":
+                search(query, _response);
+                break;
+            default:
+                error();
+        }
+    }
+}
+function insert(query, _response) {
+    let obj = JSON.parse(query["data"]);
+    let _name = obj.name;
+    let _firstname = obj.firstname;
+    let matrikel = obj.matrikel.toString();
+    let _age = obj.age;
+    let _gender = obj.gender;
+    let _studiengang = obj.studiengang;
+    let studi;
+    studi = {
+        name: _name,
+        firstname: _firstname,
+        matrikel: parseInt(matrikel),
+        age: _age,
+        gender: _gender,
+        studiengang: _studiengang
+    };
+    Database.insert(studi);
+    respond(_response, "Data received!");
+}
+function refresh(_response) {
+    Database.findAll(function (json) {
+        respond(_response, json);
+    });
+}
+function search(query, _response) {
+    let searchMatrikel = parseInt(query["searchFor"]);
+    Database.findStudent(searchMatrikel, function (json) {
+        respond(_response, json);
+    });
+}
+function error() {
+    alert("Error");
 }
 //# sourceMappingURL=Server.js.map
